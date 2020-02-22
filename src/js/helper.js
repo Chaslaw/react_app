21d4ee821bdcda54bd25e6fab59e2a8053fb08bf
Chaslaw <caslavlazic@gmail.com>
2020-02-22 21:06:11 +0100

    Initial ES6 structure

---
+++ src/js/helper.js
@@ -0,0 +1,8 @@
+const text = "This txt";
+const other = "Some other text";
+const second = "Second text";
+
+
+export {text};
+export {other};
+export {second};
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import {text} from './helper.js';
+import {other} from './helper.js';
+import {second} from './helper.js'
+
+console.log(text);
+console.log(other)
+console.log(second)
+
